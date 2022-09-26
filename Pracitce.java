2f72717f2b104d2caa0f534d4e4123f5765732c8
NelsonMaHernandez <106204327+NelsonMaHernandez@users.noreply.github.com>
2022-09-25 23:36:47 -0400

    Add files via upload

---
+++ Pracitce.java
@@ -0,0 +1,40 @@
+package practical;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Pracitce {
+
+	
+	public static void main(String[] args) {
+		
+		String[] names = {"Nelson","Mike","Wee","Wash"};
+		
+		for(int i = names.length; i>=0; i++) {
+			
+			
+		}
+		System.out.println(Arrays.toString(names));
+	
+		
+		
+		Collection<String>fruits= new HashSet<>() ;
+		
+		fruits.add("Nelson");
+		fruits.add("Mike");
+		fruits.add("Tyson");
+		
+		System.out.println(fruits);
+		
+
+	}
+		
+		
+		
+	
+	
+	
+}
+
